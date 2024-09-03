b4079194ac6b5a33f18980d2e31409e96a45e34e
HossamSalah1 <125518824+HossamSalah1@users.noreply.github.com>
2024-09-03 19:21:16 +0300

    add nav_main_footer

---
+++ react_Demo/src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+
+export default class Main extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return <h1>heloo from main componant</h1>
+    }
+}
